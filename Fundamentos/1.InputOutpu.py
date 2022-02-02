007f20d42f051e5101034425afe498c450395985
Andres Suarez <andres.suarez02@epn.edu.ec>
2022-02-01 20:24:27 -0500

    Variables

---
+++ Fundamentos/1.InputOutpu.py
@@ -0,0 +1,22 @@
+# Funcion de entrada y función de salida
+#Va a busacr las palabras reservadas
+
+#Argumento a la funcion, '' -> String
+print('Formulario')
+print('Nombre: ', end = '')
+#Input -> entrada de datos
+nombre = input();
+print('Apellido: ', end = '')
+apellido = input()
+print('Edad: ', end = '')
+edad = input()
+
+#Primer reto curso
+ciudad = input('Ingresa tu ciudad: ')
+print('El nombre ingresado es: ', nombre)
+print('El apellido ingresado es: ', apellido)
+print('Su edad es: ', edad)
+print('Tu ciudad ingresada es: ', ciudad)
+
+
+#Python es un lenguaje 100% orientado a objetos
---
+++ Fundamentos/2.Varaibles.py
@@ -0,0 +1,22 @@
+#Variable
+#No deben contener caracteres especiales, no deben empezar con mayusculas, no deben ser palabras reservadas
+
+#Tipos de variables  en python 
+#Python es un lenguaje de tipado debil
+#Inicializacion de las variables
+#4. El tipo de dat ode la variable viene dado por su inicializacion
+
+edad = 22 #int
+nombre = 'Andres'
+#La declaracion de los flotantes es con un punto
+ira = 37.2 
+mayorEdad = True;
+
+print('El tipo de dato de la variable edad es: ', type(edad))
+print('El tipo de dato ede la variable nombre es: ', type(nombre))
+print('El tipo de dato de la variable ira es: ', type(ira))
+print('El tipo de dato de la variable mayorEdad es: ', type(mayorEdad))
+
+
+
+#int 
