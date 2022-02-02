1dbc5f97895c1cfde75eb4d472203c5ad153dd23
Andres Suarez <andres.suarez02@epn.edu.ec>
2022-02-01 19:00:20 -0500

    inicio

---
+++ Fundamentos/0.HolaMundo.py
@@ -0,0 +1,11 @@
+#Nuestro primer script
+'''
+Este es un comentario largo
+'''
+
+#Entrada de datos se realiza con input
+#Salida de datos se realiza con print
+print('Formulario')
+print('Ingresa tu nombre: ')
+nombre = input()
+print('El nombre ingresado es: ',nombre)
