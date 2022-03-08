0095f0b3bb7e5cb5332dcce96a38df31be98c1f4
Andres Suarez <andres.suarez02@epn.edu.ec>
2022-03-08 18:50:43 -0500

    multiherencia

---
+++ POO/6.MultiHerencia.py
@@ -0,0 +1,33 @@
+# Multiherencia
+
+from Personaje import Personaje
+
+class Villano(Personaje):
+    def __init__(self, nombre, defecto= 'Codicioso'):
+        super().__init__(nombre)
+        self.defecto = defecto
+    
+    def destruirMundo(self):
+        print('Acabando con el mundo')
+
+class SuperHeroe(Personaje):
+    def __init__(self, nombre, virtud= 'Bueno'):
+        super().__init__(nombre)
+        self.virtud = virtud
+    
+    def saludar(self):
+        print('Salvando al mundo')
+
+
+class PersonajeNeutral(Villano, SuperHeroe):
+    def __init__(self, nombre, defecto, virtud):
+        Villano.__init__(self, nombre, defecto)
+        SuperHeroe.__init__(self, nombre, virtud)
+
+    def decirDialogo(self):
+        print('Estoy diciendo algo')
+
+personaje = PersonajeNeutral('Andres', 'Malo', 'Bueno')
+personaje.decirDialogo()
+personaje.saludar()
+
