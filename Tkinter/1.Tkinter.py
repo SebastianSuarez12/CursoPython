05e1759edc87514f1d6e1a0d9f9251e1a3a21556
Andres Suarez <andres.suarez02@epn.edu.ec>
2022-03-21 20:33:06 -0500

    Tkinter

---
+++ Tkinter/1.Tkinter.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+# Inicializar la ventana principal 
+root = tk.Tk()
+# Propiedades generales
+root.title('T1')
+# Primer parametro es width y el segudno es height
+root.resizable(True,True)
+# Escrito una sola vez, es el bucle principal
+root.mainloop()
